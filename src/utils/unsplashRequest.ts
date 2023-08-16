466b4c4b3556ac260f8cd20fbfcf392aedaaf526
Allie <alliecaton3@gmail.com>
2023-08-15 18:46:11 -0700

    Initial setup

---
+++ src/utils/unsplashRequest.ts
@@ -0,0 +1,20 @@
+import axios from 'axios'
+
+export const unsplashRequest = ({
+	method,
+	url,
+	data,
+}: {
+	method: string
+	url: string
+	data: object
+}) => {
+	axios({
+		method: method,
+		url: process.env.REACT_APP_UNSPLASH_BASE_URL + url,
+		headers: {
+			Authorization: `Client-ID ${process.env.REACT_APP_UNSPLASH_ACCESS_KEY}`,
+		},
+		data: data,
+	})
+}
