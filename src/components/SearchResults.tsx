de8d019def52f8c6d7e72a1deac1e63b8a1241db
alliecaton <alliecaton3@gmail.com>
2023-10-19 11:21:07 -0700

    Responsiveness tweaks

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, FormEvent } from 'react'
+import { useEffect, useState } from 'react'
 import axios from 'axios'
 import JSZip from 'jszip'
 import { saveAs } from 'file-saver'
